f3b005743844d9555c4efb498634086ddc0e3b00
AndreyR <Soul9160201>
2022-03-30 22:21:56 +0300

    3 commit

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -7,7 +7,7 @@ public class App {
         Book book1 = new Book("О скитаньях вечных и о Земле ", 1988, author1);
         Book book2 = new Book("Собор парижской богоматери ", 2011, author2);
         book1.setPublishingYear(1987);
-        System.out.println("book1 = " + book1.getBookName() + book1.getPublishingYear() + " author1 = " + author1.getFirstName() + author1.getLastName());
-        System.out.println("book2 = " + book2.getBookName() + book2.getPublishingYear() + " author2 = " + author2.getFirstName() + author2.getLastName());
+        System.out.println("book1 = " + book1.getBookName() + book1.getPublishingYear() + " author1 = " + book1.getAuthor().getLastName() + book1.getAuthor().getLastName());
+        System.out.println("book2 = " + book2.getBookName() + book2.getPublishingYear() + " author2 = " + book2.getAuthor().getFirstName() + book2.getAuthor().getLastName());
     }
 }
--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -3,11 +3,16 @@ package com.company;
 public class Book {
     private final String bookName;
     private int publishingYear;
-    private Author author;
+    private final Author author;
 
     public Book(String bookName, int publishingYear, Author author) {
         this.bookName = bookName;
         this.publishingYear = publishingYear;
+        this.author = author;
+    }
+
+    public Author getAuthor() {
+        return this.author;
     }
 
     public String getBookName() {
